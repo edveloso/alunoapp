da476c411477e523b08892be05488c7b70c051e6
edveloso <edveloso@gmail.com>
2020-06-20 10:27:10 -0300

    commit inicial

---
+++ src/main/java/br/edu/infnet/alunoapp/config/AppConfig.java
@@ -0,0 +1,32 @@
+package br.edu.infnet.alunoapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+//@Component
+//@Service
+//@Controller
+//@Repository	
+	
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "br.edu.infnet.alunoapp")
+public class AppConfig {
+
+	@Bean
+	public ViewResolver getViewResolver() {
+		InternalResourceViewResolver vr = new InternalResourceViewResolver();
+		vr.setViewClass(JstlView.class);
+		vr.setPrefix("/WEB-INF/views/");
+		vr.setSuffix(".jsp");
+		return vr;
+	}
+	
+	
+}
